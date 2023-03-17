83604fd7c6f907c86bb2c81fef8326931f985bac
Bra-Kwaku <kumafrancis9@gmail.com>
2023-03-16 17:14:32 -0700

    c programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,6 @@
-#include <stdlib.h>
+#include <stdio.h>
 
-/**
- * main - Entry point
- * Return: Always 1 (Success)
- */
-int main(void)
-{
-	write(2, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 61);
-	return (1);
+int main(void) { write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+
+return (1);
 }
