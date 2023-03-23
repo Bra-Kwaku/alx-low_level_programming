72a264bf7fd7a67e2b8097a861233979955ecf5e
Bra-Kwaku <kumafrancis9@gmail.com>
2023-03-23 14:32:45 -0700

    mor functions, more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,18 +4,15 @@
 * @c: Variable text
 * Return: Always 0.
 */
+
 int _isupper(int c)
 {
-
-
- if (c >= 'A' && c <= 'Z')
- {
- return (1);
- }
- else
+if (c >= 'A' && c <= 'Z')
 {
- return (0);
+return (1);
 }
-
+else
+{
+return (0);
 }
-
+}
