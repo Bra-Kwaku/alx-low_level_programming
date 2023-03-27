f2ac4b1e5f1df6a6caa0f7372d2a3ff753a69f1a
Bra-Kwaku <kumafrancis9@gmail.com>
2023-03-27 16:05:40 -0700

    Array

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes parameter and update value
+ * @n:the value to 98
+ * Return: Always o
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
