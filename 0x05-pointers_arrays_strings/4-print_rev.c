11e675bda935059c5f18ea827d07bc60f9d092f2
Bra-Kwaku <kumafrancis9@gmail.com>
2023-03-27 17:13:47 -0700

    Array

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
-/** * print_rev - imprime en reversa
-* @s: string
-* return: 0
-*/
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * Return: Always 0
+ */
 
 void print_rev(char *s)
-{ 
-int longi = 0; 
-int o; 
-while (*s != '\0') 
-{ 
-longi++; 
-s++; 
+{
+int longi = 0;
+int o;
+while (*s != '\0')
+{
+longi++;
+s++;
 }
 
