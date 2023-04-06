1adbb5078a9b201b125ff921c1bc90f8d0afe2bc
Bra-Kwaku <kumafrancis9@gmail.com>
2023-04-05 17:54:06 -0700

    RECURSION

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,20 +2,22 @@
 
 /**
  * Write a function that returns the factorial of a given number.
- * If n is lower than 0
+ * * factorial - returns the factorial of a number
+ * @n: number to return the factorial from
+ * Return: factorial of n
  *  the function should return -1 to indicate an error
  *  Factorial of 0 is 1
  */
 
 int factorial(int n)
 {
-	if(n < 0)
+	if (n < 0)
 	{
-		return -1;
+		return (-1);
 	}
-	if(n == 0)
+	if (n == 0)
 	{
-		return 1;
+		return (1);
 	}
 	return n * (factorial(n - 1));
 }
