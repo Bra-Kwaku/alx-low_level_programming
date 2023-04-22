63bf42526383c157b9a640577c0dcad17814b372
Bra-Kwaku <kumafrancis9@gmail.com>
2023-04-22 03:22:48 -0700

    Variadic Functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,8 @@
-#ifndefine "variadic_functions.h"
-#define "variadic_functions.h"
+#ifndef VAR_FUNC_H
+#define VAR_FUNC_H
 
-#include <stdarg.h>
 #include <stdio.h>
-
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
